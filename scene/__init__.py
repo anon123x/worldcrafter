4e13184fa5315aa33e36925b7a949ada424d9d72
anon123x <jianyin1968@gmail.com>
2025-08-05 23:40:05 +0800

    Add files via upload

--- scene/__init__.py
+++ scene/__init__.py
@@ -1,1 +1,25 @@
 
+import random
+
+from arguments import GSParams
+from scene.dataset_readers import readDataInfo
+from scene.gaussian_model import GaussianModel
+
+
+class Scene:
+    def __init__(self, traindata, gaussians: GaussianModel, opt: GSParams, is_sky: bool = False):
+        self.traindata = traindata
+        self.gaussians = gaussians
+        
+        info = readDataInfo(traindata, opt.white_background)
+        # random.shuffle(info.train_cameras)  # Multi-res consistent random shuffling
+        self.cameras_extent = info.nerf_normalization["radius"]
+
+        print("Loading Training Cameras")
+        self.train_cameras = info.train_cameras
+
+        self.gaussians.create_from_pcd(info.point_cloud, self.cameras_extent, is_sky=is_sky)
+        self.gaussians.training_setup(opt)
+
+    def getTrainCameras(self):
+        return self.train_cameras
